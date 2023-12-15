9fa7a53ac21d852a217627330edd6332d8e8c7e3
Timothy Campbell <tcampbel@c2r3p4.hive.fi>
2023-12-15 10:07:20 +0200

    updated header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,35 +6,24 @@
 /*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 11:51:36 by tcampbel          #+#    #+#             */
-/*   Updated: 2023/12/11 16:58:29 by tcampbel         ###   ########.fr       */
+/*   Updated: 2023/12/13 17:58:20 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-typedef struct s_list
-{
-	char	*buf;
-	struct s_list *next;
-}	t_list;
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
-t_list	*ft_lstlast(t_list *list);
-void	ft_lstadd_back(t_list **list, t_list *new);
-void	*lst_clear(t_list **list);
-void	*ft_bzero(void *s, size_t n);
-size_t	ft_strlen(const char *s);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *src);
-char	*ft_strjoin(char const *str1, char const *str2);
-char	*ft_strchr(const char *str, int c);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
+size_t	ft_strlen(char *s);
+char	*ft_strdup(char *src);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_strchr(char *str, int c);
+char	*ft_substr(char *str, size_t start, size_t len);
 #endif
